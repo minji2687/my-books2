6a488d503101894e2db5333093b9b2e18942b322
Jo Minji <mingi2687@gamil.com>
2022-01-14 16:31:51 +0900

    feat: 라우터 설정,store생성,  root reducer설정,auth 액션 생성 및 auth reducer 생성

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../types";
+// import { Redirect } from "react-router";
+// import ListContainer from "../containers/ListContainer";
+
+// export default function Home() {
+//   const token = useSelector<RootState, string | null>(
+//     (state) => state.auth.token
+//   );
+
+//   if (token === null) {
+//     return <Redirect to="/signin" />;
+//   }
+
+//   return <ListContainer />;
+// }
+
+export default function Home() {
+  return <div>Home</div>;
+}
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,20 @@
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { Redirect } from "react-router";
+// import SigninContainer from "../containers/SigninContainer";
+// import { RootState } from "../types";
+
+// export default function Signin() {
+//   const token = useSelector<RootState, string | null>(
+//     (state) => state.auth.token
+//   );
+
+//   if (token !== null) {
+//     return <Redirect to="/" />;
+//   }
+//   return <SigninContainer />;
+// }
+
+export default function Signin() {
+  return <div>"signin"</div>;
+}
